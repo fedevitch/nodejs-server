6523d81b63097ac555f49c1c81f06ee15d20cb02
fedevitch <lmfedevitch@gmail.com>
2015-10-15 19:38:15 +0300

    request algorithm changed

--- parse.js
+++ parse.js
@@ -38,97 +38,153 @@ $(document).ready(function() {
         //output += ' - FileType: ' + (file.type || 'n/a') + '<br />\n';
         output += ' - Size: ' + file.size + ' bytes<br />\n';
         output += ' - Last modified: ' + (file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a') + '<br />\n';
-
+        output += ' - Convert to CSV :  <button onclick="saveSheet()">Save</button><br/> \n';
     // read the file contents
     printTable(file);
 
     // post the results
     $('#list').append(output);
   }
-var transactions;
-
+    var transactions;
+    var data;
+    
+function setValue(row,column){
+        var getId = row.toString()+column.toString();
+        var changedField = document.getElementById(getId);
+        var value = changedField.value;
+        data[row][column] = value;
+        console.log('changed value with id: '+getId+' to:',value);
+        var checked = false;
+        if ((column === 0 || column === 1)&&(value !== '')){
+            checked = true;
+        }
+        if (column === 2){
+            checked = currencyCheck(value);
+        }
+        if (column === 3){
+            checked = isNumeric(value);
+        }
+        if(checked){
+            changedField.setAttribute('style','color:blue');
+        } else {
+            changedField.setAttribute('style','color:red');
+        }
+        checkValues();
+    }
+  
+function checkValues(){
+     var isCorrect = true;
+     var formatError = '';
+     for(var row in data) {
+         var currentRow = parseInt(row)+parseInt(1);
+         currencyField = document.getElementById(row.toString()+2);
+         amountField = document.getElementById(row.toString()+3);
+         if(currencyCheck(data[row][2])){
+                currencyField.setAttribute('style','color:black');
+             }else{
+                    currencyField.setAttribute('style','color:red');
+                    isCorrect = false;            
+                    formatError += '</p>Currency data in row '+currentRow+' is incorrect;\n</p>';            
+         }
+         if(isNumeric(data[row][3])){
+                amountField.setAttribute('style','color:black');
+            }   else{
+                amountField.setAttribute('style','color:red');
+                isCorrect = false;
+                formatError += '<p>Amount data in row '+ currentRow +' is incorrect;</p>\n';            
+         }
+         if(data[row][4]){
+                isCorrect = false;
+                formatError += 'Too many columns in file (>4)\n';
+         }
+     }
+     var out = '';
+     if(isCorrect){
+            transactions = data;
+            out+='<button onclick=sendTransactions(transactions)>Send</button> \n';
+            //console.log(isCorrect);
+        }else{
+            out+='<p>Data in file is not correct. Please edit highlighted items and try again<p>\n';
+            out+='<button onclick="checkValues()">re-check</button><br/>';
+            out+='<i>Errors: '+formatError+'</i>\n';
+            //console.log(formatError);
+        }
+     $('#control').html(out);
+ }
+    
   function printTable(file) {
     var reader = new FileReader();
     reader.readAsText(file);
     
     reader.onload = function(event){
       var csv = event.target.result;
-      var data = $.csv.toArrays(csv);
+      data = $.csv.toArrays(csv);
       var html = '';
-      var isCorrect = true;
-      var formatError = '';
-      for(var row in data) {          
-          for(var i in data[row]){              
-              if(typeof data[row][i] === "undefined"){
-                  data[row][i] = "empty value";                  
-              }
-          }
+      for(var row in data) {
         var currentRow = parseInt(row)+parseInt(1);
         html += '<tr>\r\n';
-        html += '<td>'+currentRow+'</td><td>'+data[row][0]+'</td>\r\n';
-        html += '<td>'+data[row][1]+'</td>\r\n';
-        var currencyIndex = CurrencyCodes.indexOf(data[row][2].valueOf());        
-        if((data[row][2].valueOf().length == 3)&&(currencyIndex >= 0)){
-            html += '<td>'+data[row][2]+'</td>\r\n';//currency
-            console.log(data[row][2],currencyIndex);
-        }else{            
-            isCorrect = false;            
-            formatError += '</p>Currency data in row '+currentRow+' is incorrect;\n</p>';
-            html += '<td><font color="red">'+data[row][2]+'</font></td>\r\n';
-            console.log(typeof(data[row][2]),data[row][2],currencyIndex,data[row][2].valueOf().length);
-            
-        }
-        if(isNumeric(data[row][3])){
-            html += '<td>'+data[row][3]+'</td>\r\n';//amount
-        }   else{
-            isCorrect = false;
-            formatError += '<p>Amount data in row '+currentRow+' is incorrect;</p>\n';
-            html += '<td><font color="red">'+data[row][3]+'</font></td>\r\n';
-        }
-        if(data[row][4]){
-            isCorrect = false;
-            formatError += 'Too many columns in file (>4)\n';
-        }
-//        for(var item in data[row]) {
-//          html += '<td>' + data[row][item] + '</td>\r\n';
-//        }
+        html += '<td>'+currentRow+'</td><td><input type=text id="'+ row + 0 +'" onchange="setValue('+ row +','+ 0 +')" value='+data[row][0]+'></td>\r\n';
+        html += '<td><input type=text id="'+ row + 1 +'" onchange="setValue('+ row +','+ 1 +')" value='+data[row][1]+'></td>\r\n';               
+        html += '<td><input type=text id="'+row+2+'" onchange="setValue('+ row +','+ 2 +')" value='+data[row][2]+'></td>\r\n';//currency
+        //console.log(data[row][2],currencyIndex);        
+        html += '<td><input type=text id="'+ row + 3 +'" onchange="setValue('+ row +','+ 3 +')" value='+data[row][3]+'></td>\r\n';//amount
         html += '</tr>\r\n';       
-      }
-      var out = '';
-      if(isCorrect){
-            transactions = data;
-            out+='<button onclick=sendTransactions(transactions)>Send</button> \n';
-            //console.log(isCorrect);
-        }else{
-            out+='<p>Data in file is not correct. Please edit highlighted items and try again<p>\n';
-            out+='<i>Errors: '+formatError+'</i>\n';
-            //console.log(formatError);
-        }
+      }    
+        //console.log(html);
       $('#contents').html(html);
-      $('#control').html(out);
+      checkValues();      
     };
-    reader.onerror = function(){ alert('Unable to read ' + file.fileName); };
+    reader.onerror = function(){ alert('Unable to read ' + file.fileName); };   
+    
   }
 
 function sendTransactions(transactions){
-    alert("sending transaction");
+    alert("sending transactions");
+    var jsonData = new Array();
+    function transaction(account, description, currency, amount, time){
+        this.account = account;
+        this.description = description;
+        this.currency = currency;
+        this.amount = amount;
+        this.time = time;
+    }
     for (var row in transactions){
+        var currentTime = new Date();
+        transactions[row][4] = currentTime.getDate()+'.'+parseInt(currentTime.getMonth()+1)+'.'+currentTime.getFullYear()+'-'
+            +currentTime.getHours()+':'+currentTime.getMinutes()+':'+currentTime.getSeconds()+'.'+currentTime.getMilliseconds();
+        jsonData[row] = new transaction(transactions[row][0],transactions[row][1],transactions[row][2],
+                                        transactions[row][3],transactions[row][4]);
+    /*
     $.post('http://127.0.0.1:64530/transaction?account='+transactions[row][0]+'&descr='+transactions[row][1]+'&currency='+transactions[row][2]+'&amount='+transactions[row][3])
+    $.post('http://192.168.0.100:64530/transaction?account='+
+            transactions[row][0]+'&descr='+transactions[row][1]
+            +'&currency='+transactions[row][2]
+            +'&amount='+transactions[row][3]
+            +'&time='+currentTime.getDate()+'.'+parseInt(currentTime.getMonth()+1)+'.'+currentTime.getFullYear()+'-'
+            +currentTime.getHours()+':'+currentTime.getMinutes()+':'+currentTime.getSeconds()+'.'+currentTime.getMilliseconds())    
     .done(function() {
         alert( "Data uploaded");
-  });
+     
+    });  */       
+        
     }
+    $.post('http://192.168.0.100:64530/send',{data: jsonData}, function(data){
+        alert(data);
+    });
 }
 
 function currencyCheck(charCode){
-    for (var currentCode in CurrencyCodes){
-        if(charCode.valueOf() === CurrencyCodes[currentCode].valueOf()){
-            return currentCode;
-        }
+    if ((charCode.valueOf().length == 3)&&(CurrencyCodes.indexOf(charCode) >= 0)){
+            return true;
     }
-    return -1;
+    return false;
 }
 
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function saveSheet(){
+    //document.write(data.join(', '));
+    document.write($.csv.fromArrays(data));
 }
--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 var express = require('express');
-var hbs = require('express-hbs');
+//var hbs = require('express-hbs');
 var app = express();
 var url = require('url');
-var urlParsed = url.parse(req.url, true);
 var mongojs = require('mongojs');
 var db = mongojs('localhost/transactions');
 var transactionsCollection = db.collection('transactions');
 
-app.post('/transaction', function(req, res) {
-   if(urlParsed.query.account && urlParsed.query.descr && urlParsed.query.currency && urlParsed.query.amount){	
-        transactionsCollection.insert({account: urlParsed.query.account, description: urlParsed.query.descr, currency: urlParsed.query.currency, amount:urlParsed.query.amount});
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/transaction', function(req, res) {//deprecated
+    var receivedData = req.var;
+   var urlParsed = url.parse(req.url, true);
+   if(urlParsed.query.account && urlParsed.query.descr && urlParsed.query.currency && urlParsed.query.amount && urlParsed.query.time){	
+        transactionsCollection.insert({account: urlParsed.query.account, description: urlParsed.query.descr, currency: urlParsed.query.currency, amount:urlParsed.query.amount, dateTime:urlParsed.query.time});
         res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-        console.log("Recorded to db transaction: account : " + urlParsed.query.account + ", description: " + urlParsed.query.descr + ",currency: " + urlParsed.query.currency + ", amount: "+urlParsed.query.amount);
+        console.log("Recorded to db transaction: account : " + urlParsed.query.account + ", description: " + urlParsed.query.descr + ",currency: " + urlParsed.query.currency + ", amount: "+urlParsed.query.amount + ", time: "+urlParsed.query.time);
         res.end('Data accepted');  
   }
 });
 
+app.post('/send', function(req, res) {    
+    console.log(req.body);	
+    transactionsCollection.insert(req.body.data);
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+    res.write(req.body.length+" transactions recorded to db succesfully");
+    console.log(req.body.data.length+" transactions recorded to db succesfully");
+    res.end('Data accepted');
+});
+
 app.get('/transactions', function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+    console.log('Request list from: '+req.headers.host);
     transactionsCollection.find(function (err, transactions) {
         // docs is an array of all the documents in collection
         if(err){
@@ -27,11 +41,18 @@ app.get('/transactions', function(req, res) {
         //resHtml += '<a href="index.html" name="back">Back</a>';
         resHtml += '<table id="contents" style="width:100%; height:400px;" border>';
         resHtml += '<tr>\r\n';
-        resHtml +='<td>id</td>\r\n<td>account</td>\r\n<td>description</td>\r\n<td>currency</td>\r\n<td>amount</td></tr>';
+        resHtml +='<td>#</td>\r\n<td>account</td>\r\n<td>description</td>\r\n<td>currency</td>\r\n<td>amount</td></tr>';
         for (var row in transactions){
             resHtml += '<tr>\r\n';                    
             for(var item in transactions[row]) {
-              resHtml += '<td>' + transactions[row][item] + '</td>\r\n';
+                //console.log('output [ '+row+' : '+item+' ]: '+transactions[row][item]);
+				if (item === '_id'){
+                    resHtml += '<td>' + parseInt(parseInt(row)+parseInt(1)) + '</td>\r\n';
+                    continue;
+                }else{
+                    resHtml += '<td>' + transactions[row][item] + '</td>\r\n';
+                }
+				
             }
             resHtml += '</tr>\r\n';
 
